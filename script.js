a54e4d8307ed6bb6cb606d1d8dfffe887f7c533a
boticelli <kandelakitina@gmail.com>
2025-04-19 19:41:54 +0300

    Add error

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function createGrid(parent, n) {
+    if (n > 100) throw Error("Width's too high. Only <100 is accepted");
     let container = document.querySelector(parent);
     container.innerHTML = "";
 
     const blockSize = 100 / n;
+
     for (let i = 0; i < n * n; i++) {
         let div = document.createElement("div");
         div.classList.add("block");
@@ -12,3 +14,17 @@ function createGrid(parent, n) {
 }
 
 createGrid(".container", 50);
+
+let container = document.querySelector(".container");
+container.addEventListener("mouseover", (e) => {
+    let div = e.target.closest(".block");
+    if (!div) return;
+    if (!container.contains(div)) return;
+    e.target.style.background = "black";
+})
+
+let button = document.querySelector(".button");
+button.addEventListener("click", (e) => {
+    let width = +prompt("Enter width", 5);
+    createGrid(".container", width);
+})
